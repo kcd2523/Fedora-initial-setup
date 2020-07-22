9e26bd03c4ce827315dc5f53a3dff90ab7ae0db8
kcd2523 <58370460+kcd2523@users.noreply.github.com>
2020-07-23 01:46:28 +0700

    Add files via upload

---
+++ Fedora-Unistall-setup.sh
@@ -0,0 +1,1 @@
+sudo dnf remove cheese gnome-boxes gnome-contacts gnome-dictionary gnome-user-docs gnome-getting-started-docs gnome-initial-setup gnome-maps gnome-photos gnome-software gnome-wearther simple-scan totem yelp

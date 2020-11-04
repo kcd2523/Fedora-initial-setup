a22c8fce53fdffd53980e0aafcd65c4effac2e55
kcd2523 <58370460+kcd2523@users.noreply.github.com>
2020-11-04 20:02:40 +0700

    Update Fedora-disableservices-setup.sh

--- Fedora-disableservices-setup.sh
+++ Fedora-disableservices-setup.sh
@@ -1,1 +1,1 @@
-sudo systemctl --user mask evolution-addressbook-factory.service evolution-calendar-factory.service evolution-source-registry.service
+systemctl --user mask evolution-addressbook-factory.service evolution-calendar-factory.service evolution-source-registry.service
